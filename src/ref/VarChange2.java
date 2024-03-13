f8cd0af5442557a2f9b0086ea9b2e8b80be91f95
doyoon <doyoun0527@naver.com>
2024-03-13 20:41:15 +0900

    기본형 vs 참조형 - 변수 대입

--- src/ref/VarChange2.java
+++ src/ref/VarChange2.java
@@ -4,7 +4,7 @@ public class VarChange2 {
     public static void main(String[] args) {
 
         Data dataA = new Data();
-        dataA.value = 10;
+        dataA.value = 10; //객체를 생성하면 참조값이 생성이 되고, 이 참조값을 통해 객체에 접근해서 객체를 사용한다.
         Data dataB = dataA; //인스턴스가 복사돼서 만들어지는 것이 아니라 , 참조값만 읽어서 B 에 들어가는것.
 
         System.out.println("dataA 참조값=" + dataA);

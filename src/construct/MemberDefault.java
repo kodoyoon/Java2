b2bbafb86a958a8bacf871515c33ed3eca4408a1
doyoon <doyoun0527@naver.com>
2024-03-18 16:20:25 +0900

    기본 생성자

---
+++ src/construct/MemberDefault.java
@@ -0,0 +1,9 @@
+package construct;
+
+public class MemberDefault {
+    String name;
+
+    MemberDefault(){
+        //기본 생성자
+    }
+ }
--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -5,6 +5,10 @@ public class MemberInit { //4, 5, 6 은 멤버변수
     int age;
     int grade;
 
+//    MemberInit(){ // 기본 생성자
+//
+//    }
+
     //추가
     void initMember(String name, int age, int grade) {
         this.name = name;  //자기 자신의 인스턴스를 가르킴, this 가 없으면 name = name 이름이 똑같음

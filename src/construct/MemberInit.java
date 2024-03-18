ad0eae18f797b056d09a638ec05e6ce9f4697701
doyoon <doyoun0527@naver.com>
2024-03-18 15:32:08 +0900

    this

--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -1,14 +1,14 @@
 package construct;
 
 public class MemberInit {
-    String name;
+    String name; //얘네 값을 넣어주고 싶은거임
     int age;
     int grade;
 
     //추가
     void initMember(String name, int age, int grade) {
-        this.name = name;  //자기 자신의 인스턴스를 가르킴
+        this.name = name;  //자기 자신의 인스턴스를 가르킴, this 가 없으면 name = name 이름이 똑같음
         this.age = age;
         this.grade = grade;
     }
-}
+} // 가까운 scope 이 우선순위를 가짐

aec71b8973b460297e9d38ae0ba9d9481e6cff36
doyoon <doyoun0527@naver.com>
2024-03-19 14:45:32 +0900

    접근 제어자 이해1

--- src/access/SpeakerMain.java
+++ src/access/SpeakerMain.java
@@ -11,6 +11,11 @@ public class SpeakerMain {
         speaker.volumeUp();
         speaker.showVolume();
 
+        //필드에 직저버 접근
+        System.out.println("volume 필드 직접 접근 수정");
+        speaker.volume = 200;
+        speaker.showVolume();
+
 
     }
 }

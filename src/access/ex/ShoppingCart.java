53c90c2a41c5ab3eb2052266e728f83f67a0ef07
doyoon <doyoun0527@naver.com>
2024-03-20 19:24:40 +0900

    접근 제어자 문제와 풀이 수정

--- src/access/ex/ShoppingCart.java
+++ src/access/ex/ShoppingCart.java
@@ -2,7 +2,7 @@ package access.ex;
 
 public class ShoppingCart {
     private Item [] items = new Item[10]; //아이템을 담을 수 있는 배열
-    private int itemCount; //장바구니에 담을 수 있는 아이템의 개수
+    private int itemCount; //배열에 저장된 아이템의 개수 , 초기값이 0
 
     public void addItem(Item item) {
         if(itemCount >= items.length){

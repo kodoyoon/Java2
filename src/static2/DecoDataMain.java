bf65053942e41d009195e00fcd09adf3fb073cfd
doyoon <doyoun0527@naver.com>
2024-03-21 16:14:29 +0900

    static import

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,9 +1,15 @@
 package static2;
 
+import static static2.DecoData.*;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
+
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -13,7 +19,15 @@ public class DecoDataMain {
         DecoData data2 = new DecoData();
         data2.instanceCall();
 
-        DecoData.staticCall(data1);
+        staticCall(data1);
+
+        //추가
+        //인스턴스를 통한 접근
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        //클래스를 통한 접근
+        staticCall();
     }
 }
 
